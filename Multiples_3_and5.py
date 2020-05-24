ac9f794591907c802572e32333f240675e0a4d21
JCarter111 <48532225+JCarter111@users.noreply.github.com>
2020-05-24 16:18:27 +0100

    Create Multiples_3_and5.py

---
+++ Multiples_3_and5.py
@@ -0,0 +1,24 @@
+# Kata practice from codewars
+# https://www.codewars.com/kata/514b92a657cdc65150000006/train/python
+
+# import the Python unit test module
+import unittest
+
+def solution(number):
+# find sum of all the multiples of 3 and 5
+# below the number provided
+# e.g. number = 10
+# multiples of 5 are 5 (note below the number 10)
+# multipes of 3 are 3,6,9
+# total sum is 5 + 3 + 6 + 9 = 23
+
+# divide number - 1 by 5
+
+
+
+
+# tests
+# example test provided for the kata
+test.describe("Multiples of 3 and 5")
+test.it("should handle basic cases")
+test.assert_equals(solution(10), 23)
