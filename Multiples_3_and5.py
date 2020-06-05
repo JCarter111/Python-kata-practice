30590b1172007f3779e188d3cb29e84a58c63901
JCarter111 <48532225+JCarter111@users.noreply.github.com>
2020-06-05 18:45:42 +0100

    Update Multiples_3_and5.py
    Upated code to handle cases where number provided is negative.  Added error handling if an integer is not provided for number.

--- Multiples_3_and5.py
+++ Multiples_3_and5.py
@@ -12,11 +12,19 @@ def countMultiples3And5(number):
 # multipes of 3 are 3,6,9
 # total sum is 5 + 3 + 6 + 9 = 23
 
-# check that the number supplied is an integer
-# if it is not, return zero
-#    if not(isinstance(number,int)):
-#        return 0
+# number must be an integer
+# number can be negative
+# if number is not an integer
+# raise an error
+# note Boolean value for number is interpreted as 0 or 1
+# raise error if Boolean value provided
+    if not(isinstance(number,int)) or isinstance(number,bool):
+       raise TypeError("Please provide an integer number")
+ 
+
 # loop counting up from 3 to the number
+# or down from -3 to number if number is negative
+
 # in each loop divide number by 3 and by 5
 # if the remainder is zero in either case, add the current 
 # number to the total score
@@ -24,23 +32,37 @@ def countMultiples3And5(number):
     # initially set store of total multiples to zero    
     totalMultiples = 0
 
-    # set counter for loop to number, start checking at value 3
-    loopCounter = 3
+    
 
-    #loop through all integers less than number
-    while loopCounter < number:   
-        if loopCounter%3 == 0 or loopCounter%5 == 0:
-            totalMultiples = totalMultiples + loopCounter
-        loopCounter = loopCounter + 1
+    # if number is less than 3 and greater than -3 
+    # there are no multiples of 3 and 5
+    # no further processing required
 
+    if number >= 3:  
+        # set counter for loop to number, start checking at value 3
+        #loopCounter = 3 
+        # loop through integers starting at 3 and less than number
+        for loopCounter in range(3,number):   
+            if loopCounter%3 == 0 or loopCounter%5 == 0:
+                totalMultiples = totalMultiples + loopCounter
+    elif number <= -3:
+        # set counter for loop to negative number, start checking a value - 3
+        #loopCounter = -3
+        # loop through negative integers starting at -3 and greater than negative number
+        for loopCounter in range(-3,number,-1):
+            if loopCounter%3 ==0 or loopCounter%5 ==0:
+                totalMultiples = totalMultiples + loopCounter
     return totalMultiples
 
 
 # tests
+# using unittest to run testing
+
 # example test provided for the kata
 #test.describe("Multiples of 3 and 5")
 #test.it("should handle basic cases")
 #test.assert_equals(solution(10), 23)
+
 
 class multiple3And5Tests(unittest.TestCase):
 
@@ -67,64 +89,25 @@ class multiple3And5Tests(unittest.TestCase):
     # test for a large number
     def test_thirtyeight_returns_358(self):
         self.assertEqual(countMultiples3And5(38),329)
-    # test that the code works with a non integer number
-    def test_noninteger_returns_multiples(self):
-        self.assertEqual(countMultiples3And5(9.5),23) 
+    # test negative number
+    def test_minusthirtyeight_returns_minus358(self):
+        self.assertEqual(countMultiples3And5(-38),-329)
+    # test number = 1
+    def test_one_returns_zero(self):
+        self.assertEqual(countMultiples3And5(1),0)
 
-    # test single t34
-    #def test_one_t34_costs_99(self):
-    #    self.assertEqual(checkout(["t34"]),99) 
-
-    # test a99, b15
-    #def test_one_a99_and_one_b15_costs_80(self):
-    #    self.assertEqual(checkout(["a99","b15"]),80)
-
-    # test c40 and t34
-    #def test_one_c40_and_one_t34_costs_159(self):
-    #    self.assertEqual(checkout(["c40","t34"]),159)
-    # test b15 and t34
-    #def test_one_b15_and_one_t34_costs_129(self):
-    #    self.assertEqual(checkout(["b15","t34"]),129)
-
-    # test one of each item in list
-    #def test_one_a99_one_t34_and_one_c40_and_one_b15_costs_189(self):
-    #    self.assertEqual(checkout(["t34","c40","b15","a99"]),239)
-
-    # test that discount is applied to 2 * b15 when no other items present
-    #def test_two_b15_costs_45(self):
-    #    self.assertEqual(checkout(["b15","b15"]),45)
-
-    # test that discount is applied to 3 * a99 when no other items present
-    #def test_three_a99_costs_130(self):
-    #    self.assertEqual(checkout(["a99","a99","a99"]),130)
-
-    # test that discount is applied to 2 * b15 when other items present
-    #def test_two_b15_and_c40_costs_105(self):
-    #    self.assertEqual(checkout(["b15","c40","b15"]),105)
-
-    # test that discount is applied to 3 * a99 when other items present
-    #def test_three_a99_and_c40_costs_190(self):
-    #    self.assertEqual(checkout(["a99","c40","a99","a99"]),190)
-
-    # test deduction applied to three items when 4 * a99 in the checkout items
-    #def test_four_a99(self):
-    #    self.assertEqual(checkout(["a99","a99","a99","a99"]),180)
-
-    # test deduction applied to two items when 3 * b15 in the checkout items
-    #def test_three_b15(self):
-    #    self.assertEqual(checkout(["b15","b15","b15"]),75)
-
-    # test deduction applied more than once e.g. 7 * a99, 11 * b15
-    # items in random order
-    #def test_seven_a99_eleven_b15(self):
-    #    self.assertEqual(
-    #        checkout(["a99","b15","b15","b15","a99","b15","b15","a99","b15","a99","b15","b15","a99","a99","b15","b15","a99","b15"]),
-    #        565)
-
-    # test deduction applied more than one, with other items in the basket
-    #def test_nine_a99_seven_b15_two_c40_three_t34(self):
-    #    self.assertEqual(
-    #        checkout(["c40","a99","t34","b15","b15","a99","b15","c40","a99","b15","a99","b15","t34","t34","b15","a99","a99","b15","a99","a99","a99"]),
-    #        972)
+    # error handling tests
+    # test that an error is raised if a non-integer number is 
+    # provided
+    def test_noninteger_raises_error(self):
+        with self.assertRaises(TypeError):
+            countMultiples3And5(9.5) 
+        with self.assertRaises(TypeError):
+            countMultiples3And5("hello")
+        with self.assertRaises(TypeError):
+            countMultiples3And5([6,7])
+        with self.assertRaises(TypeError):
+            countMultiples3And5(True)
+    
 if __name__ == "__main__":
     unittest.main()
